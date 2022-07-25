db986b084833a83b28ac29a95d710dc86c4b16b2
atyhamos <atyhalive@gmail.com>
2022-07-25 16:54:55 +0800

    Add alt for volunteer image

--- src/components/volunteer/Volunteer.component.jsx
+++ src/components/volunteer/Volunteer.component.jsx
@@ -3,13 +3,14 @@ import { useParams } from 'react-router-dom'
 import { VolunteersContext } from '../../contexts/VolunteersContext'
 import BlankPicture from '../../images/blank-profile.svg'
 import './Volunteer.component.scss'
+
 const Volunteer = () => {
   const { volunteersMap } = useContext(VolunteersContext)
   const volunteerId = useParams().id
   const { name } = volunteersMap.get(volunteerId)
   return (
     <div className='volunteer-container'>
-      <img src={BlankPicture} />
+      <img src={BlankPicture} alt={name} />
       <h2>{name}</h2>
     </div>
   )
